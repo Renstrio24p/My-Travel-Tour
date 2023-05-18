0d06adb77fabed6f5090e6aa7598958ab460ff44
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-05-18 23:42:13 +0800

    add responsive

--- Travel-Journal/components/travel-card.jsx
+++ Travel-Journal/components/travel-card.jsx
@@ -5,7 +5,11 @@ export default function TravelCard(props){
         <div className="travel-card">
             <img src={props.imageUrl} alt="a landscape picture" className="image" />
             <div className="card-content">
-                    <p className="card-location"><i className={props.icon}></i> {props.location} <a href="#0" className="location">View on Google Maps</a> </p>       
+                    <p className="card-location">
+                        <i className={props.icon}></i>
+                         {props.location} 
+                         <a href="#0" className="location">View on Google Maps</a>
+                     </p>       
                 <h1 className="title">{props.title}</h1>
                 <p className="date">{props.startDate} - {props.endDate}</p>
                 <p className="description">{props.description}</p>
